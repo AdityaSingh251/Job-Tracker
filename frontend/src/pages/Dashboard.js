1393e2fa10ad5694c5baa9c2a762bdac5f545b5b
Aditya Singh <84238810+AdityaSingh251@users.noreply.github.com>
2026-02-17 13:10:41 +0530

    Update API endpoint URLs in Dashboard.js

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -27,7 +27,7 @@ export default function Dashboard() {
         return;
       }
 
-      const res = await axios.get("https://job-tracker-ikyfrontend.vercel.app/api/jobs", {
+      const res = await axios.get("https://job-tracker-backend-lovatbackend.vercel.app/api/jobs", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -69,7 +69,7 @@ export default function Dashboard() {
       }
 
       await axios.post(
-        "https://job-tracker-ikyfrontend.vercel.app/api/jobs",
+        "https://job-tracker-backend-lovatbackend.vercel.app/api/jobs",
         { company, role, status },
         {
           headers: {
@@ -125,7 +125,7 @@ export default function Dashboard() {
         return;
       }
 
-      await axios.delete(`https://job-tracker-ikyfrontend.vercel.app/api/jobs/${id}`, {
+      await axios.delete(`https://job-tracker-backend-lovatbackend.vercel.app/api/jobs/${id}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
