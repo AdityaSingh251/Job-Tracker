a41750200cd64477988483727fa08d9d447f779b
Aditya Singh <84238810+AdityaSingh251@users.noreply.github.com>
2026-02-17 13:39:56 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -8,25 +8,14 @@ const jobRoutes = require("../routes/jobRoutes");
 
 const app = express();
 
-// ✅ connect db (only once)
 connectDB();
 
 app.use(cors({
-  origin: "https://job-tracker-ikyfrontend.vercel.app",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://job-tracker-ikyfrontend.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  
-  next();
-});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
