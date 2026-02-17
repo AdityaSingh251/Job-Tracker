2025560dfc3e3fe20ff415f05ff87b0fb6dbeb59
Aditya Singh <84238810+AdityaSingh251@users.noreply.github.com>
2026-02-17 13:01:29 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
     setMsg("");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://job-tracker-ikyfrontend.vercel.app/api/auth/login", {
         email,
         password,
       });
